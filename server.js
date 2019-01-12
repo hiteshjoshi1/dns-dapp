9a7567ae39621addb07b37ec72bca2b944e33cef
Hitesh <hitesh.joshi@consensys.net>
2019-01-12 21:32:35 +0800

    first deploy

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
+// server.js
 const path = require('path');
 
+const express = require('express');
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
